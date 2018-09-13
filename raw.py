bc60272ff56d9630524e2f2b950071e3524b3821
Edward Newgate <badaodaica1991@gmail.com>
2018-09-13 17:01:41 +0700

    100s

--- raw.py
+++ raw.py
@@ -1,11 +1,12 @@
+from collections import Counter
+from timeit import default_timer as timer
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 import nltk
 from nltk.corpus import wordnet
-
 from nltk.corpus import stopwords
-stopWords = stopwords.words('english')
+
 
 def n_max_elements(list, n, index_list, result_list):
     clone = list[:]
@@ -50,6 +51,7 @@ def handleEmpty(target):
     return temp
 
 def handleStopWords(target):
+    stopWords = stopwords.words('english')
     temp = []
     for i in target:
         if i not in stopWords:
@@ -78,71 +80,112 @@ def synonym(list, number):
 # str = ' '.join(temp)
 # print(str, "\n", temp)
 
-searchString = input("Give me your string:\n")
-searchList = searchString.split(' ')
-
-synList = synonym(searchList, 3)
-synString = ' '.join(synList)
-
-newsgroups_train = fetch_20newsgroups(subset='train')
-
-# jaccardList = []
-cosineList = []
-cosineSynList = []
 
 # a jap bike and call myself axis motors tuba irwin
+def similarity(synNum, filterNum):
 
-count = 0
-for i in range(len(newsgroups_train.data)):
-    line = newsgroups_train.data[i]
+    searchString = input("Give me your string:\n")
+    searchList = searchString.split(' ')
 
-    line = handleSpecialChar(line)
-    array = line.lower().split(' ')
-    array = list(array)
-    array = handleEmpty(array)
-    array = handleStopWords(array)
-    line = ' '.join(array)
+    synList = synonym(searchList, synNum)
+    synString = ' '.join(synList)
+    newsgroups_train = fetch_20newsgroups(subset='train')
 
-    # jac = jaccard(searchList, array)
-    # jaccardList.append(jac)
+    # jaccardList = []
+    # cosineList = []
+    # cosineSynList = []
+    dict = {}
+    dictSyn = {}
 
-    combine = [searchString, line]
-    vectors = TfidfVectorizer().fit_transform(combine)
-    array_cos = cosine_similarity(vectors[0], vectors[1])[0][0]
-    cosineList.append(array_cos)
+    count = 0
+    for i in range(len(newsgroups_train.data)):
+        line = newsgroups_train.data[i]
 
-    combineSyn = [synString, line]
-    vectors_syn = TfidfVectorizer().fit_transform(combineSyn)
-    array_cos_syn = cosine_similarity(vectors_syn[0], vectors_syn[1])[0][0]
-    cosineSynList.append(array_cos_syn)
+        line = handleSpecialChar(line)
+        array = line.lower().split(' ')
+        array = list(array)
+        array = handleEmpty(array)
+        array = handleStopWords(array)
+        line = ' '.join(array)
 
-    if array_cos > 0:
-        count += 1
+        # jac = jaccard(searchList, array)
+        # jaccardList.append(jac)
 
-index_list = []
-result_list = []
-# n_max_elements(jaccardList, 5, index_list, result_list)
-n_max_elements(cosineList, 10, index_list, result_list)
+        combine = [searchString, line]
+        vectors = TfidfVectorizer().fit_transform(combine)
+        array_cos = cosine_similarity(vectors[0], vectors[1])[0][0]
+        # cosineList.append(array_cos)
+        dict[i] = array_cos
 
-index_list_syn = []
-result_list_syn = []
-n_max_elements(cosineSynList, 10, index_list_syn, result_list_syn)
+        combineSyn = [synString, line]
+        vectors_syn = TfidfVectorizer().fit_transform(combineSyn)
+        array_cos_syn = cosine_similarity(vectors_syn[0], vectors_syn[1])[0][0]
+        # cosineSynList.append(array_cos_syn)
+        dictSyn[i] = array_cos_syn
 
-print("Number of similarity = 0: ", count)
+        if array_cos > 0:
+            count += 1
 
-print(list(set(index_list).intersection(index_list_syn)))
 
-print(index_list)
-print(index_list_syn)
+    # index_list = []
+    # result_list = []
+    # n_max_elements(jaccardList, 10, index_list, result_list)
+    # n_max_elements(cosineList, 200, index_list, result_list)
 
-# print("jaccardList", len(jaccardList))
-# print("cosineList: ", len(cosineList))
-# print("data: ", len(newsgroups_train.data))
+    # index_list_syn = []
+    # result_list_syn = []
+    # n_max_elements(cosineSynList, 200, index_list_syn, result_list_syn)
 
-# st = handleSpecialChar(newsgroups_train.data[8832])
-# lt = st.lower().split(' ')
-# lt = list(lt)
-# lt = handleEmpty(lt)
-# print(lt)
+    # print("Number of similarity = 0: ", count)
+    # print(index_list)
+    # print(index_list_syn)
+    # print(list(set(index_list).intersection(index_list_syn)))
+
+
+    for key in list(dict):
+        if dict[key] == 0:
+            del dict[key]
+
+    for key in list(dictSyn):
+        if dictSyn[key] == 0:
+            del dictSyn[key]
+
+    print(dict)
+    print(dictSyn)
+
+    resultDict = {}
+    for item in dict.keys():
+        if item in dictSyn.keys():
+            resultDict[item] = dict[item]
+
+    print(resultDict)
+    print(len(resultDict))
+
+    c = Counter(resultDict)
+    mc = c.most_common(filterNum)
+    print(mc)
+
+    # print("jaccardList", len(jaccardList))
+    # print("cosineList: ", len(cosineList))
+    # print("data: ", len(newsgroups_train.data))
+
+    # st = handleSpecialChar(newsgroups_train.data[8832])
+    # lt = st.lower().split(' ')
+    # lt = list(lt)
+    # lt = handleEmpty(lt)
+    # print(lt)
+
+start = timer()
+similarity(3, 10)
+end = timer()
+time = end - start
+print(time, " seconds")
+
+f = open("records.txt", "a")
+f.write()
+f.close()
+
+
+
 
 
